6bd261d739cde8e1e0a2af8238b4eed07e040824
Tom-the-Bomb <thello522@gmail.com>
2022-12-02 22:38:39 -0500

    many frontend updates

--- frontend/index.js
+++ frontend/index.js
@@ -1,44 +1,68 @@
 function main() {
+    document.addEventListener('paste', (event) => {
+        let files = event.clipboardData.files;
+        if (files) {
+            fileInput.files = files;
+            fileInputHandler();
+        }
+    });
+
+    doFormValidation();
+}
+
+function doFormValidation() {
+    const forms = document.querySelectorAll('.needs-validation');
+    Array.prototype.slice.call(forms)
+        .forEach((form) => {
+            form.addEventListener('submit', (event) => {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+                form.classList.add('was-validated')
+            }, false)
+        });
+}
+
+async function formHandler(event) {
+    event.preventDefault();
     const fnSelect = document.getElementById('function-select');
     const fileInput = document.getElementById('upload');
+    const file = fileInput.files[0];
 
-    if (fileInput) {
-        fileInput.addEventListener('change', async () => {
-            let file = fileInput.files[0];
-            if (file) {
-                let bytes = await file.arrayBuffer();
-                
-                if (fnSelect) {
-                    let selected = fnSelect.options[fnSelect.selectedIndex];
-                    if (selected.value === 'Select a function') {
-                        return alert('Select a function');
-                    }
-                    await makeRequest(selected.value, bytes);
-                } else {
-                    return alert('Something went wrong');
-                }
-            }
-        }, false)
+    if (file) {
+        if (fnSelect) {
+            const selected =
+                fnSelect.options[fnSelect.selectedIndex];
+            await makeRequest(selected.value, file);
+        } else {
+            return alert('Something went wrong');
+        }
     }
 }
 
 async function makeRequest(endpoint, bytes) {
+    const output = document.getElementById('output-container');
+    output.innerHTML =
+        `<div class="loading"></div>
+        <p id="processing">Processing image...</p>`;
+
     let data = new FormData();
     data.append('image', bytes);
-    let payload = {
+    const payload = {
         method: 'POST',
         body: data,
     };
 
     let response = await fetch(`/${endpoint}`, payload);
     if (response.ok) {
-        const output = document.getElementById('output-container');
-        let url = URL.createObjectURL(
+        const url = URL.createObjectURL(
             await response.blob()
         );
-        output.innerHTML = `<img id="output-image" src="${url}"/>`;
+        output.innerHTML =
+            `<img class="lg-shadow" id="output-image" src="${url}" alt="output image"/>`;
     } else {
-        alert(`${response.status}: ${response.body}`);
+        alert(`${response.status}: Something went wrong`);
     }
 }
 
